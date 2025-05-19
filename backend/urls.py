4e5f9d72f69ada84ce137e0f0245817f0a826c06
LVS57 <vincentgrey57@gmail.com>
2025-05-19 17:55:03 +0800

    Test: Confirm Render auto-deploy

--- backend/urls.py
+++ backend/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
+
+def index(request):
+    return HttpResponse("Render is connected to GitHub ✅")
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
