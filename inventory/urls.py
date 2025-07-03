78a7fe703f726d92c503c8943290837216bf7f32
SapphireShield57 <lgsingayanshs@gmail.com>
2025-07-03 17:53:13 +0800

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('product/id/<int:pk>/', views.product_detail, name='product-detail'),
     path('product/qr/<str:qr_code>/', views.get_product_by_qr, name='get_product_by_qr'),
     path('product/', views.list_products, name='product-list'),
+    path('product/stock-update/<int:pk>/', views.stock_update, name='stock-update'),
+    path('product/history/<int:pk>/', views.product_history, name='product-history'),
+    path('product/total-stock/', views.total_stock, name='total-stock'),
 ]
 
 
